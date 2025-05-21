8ddf4cb9443143c23315abd5b11c353fe9a9bc3e
John Baumert <baumert.john@gmail.com>
2025-05-21 17:26:56 -0600

    Add input box. Code cleanup.

--- freesimplegui/main.py
+++ freesimplegui/main.py
@@ -2,13 +2,26 @@
 
 import FreeSimpleGUI as sg
 
-layout = [[sg.Text("Hello, World!"), sg.Button("Click Me")]]
-window = sg.Window("My First Gui", layout)
+# layout defines what is in the window
+layout = [
+    [sg.Text("Hello, World!")],
+    [sg.Button("Click Me")],
+    [sg.Text("Enter your name:")],
+    [sg.InputText(key="name")],
+    [sg.Button("Greet Me")],
+]
+# window defines attributes about the window
+window = sg.Window("My First GUI", layout, size=(400, 400))
 
+# Event loop needed to keep window open and interact
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
     elif event == "Click Me":
         print("Button clicked!")
+    elif event == "Greet Me":
+        name = values["name"]
+        print(f"Hello, {name}!")
+        continue
     window.close()
