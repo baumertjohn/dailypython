4bf595bb2f5206644e5ec7c144644d28315b998d
John Baumert <baumert.john@gmail.com>
2025-05-27 17:49:38 -0600

    First commit - Password Generator GUI

---
+++ password_generator_gui/main.py
@@ -0,0 +1,20 @@
+# Build a Password Generator GUI with Python
+# Function for password creation based on user defined length
+# Add optional description input
+# Save to file "passwords.txt"
+
+
+def create_password(length):
+    pass
+
+
+def write_to_file(passwords):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
