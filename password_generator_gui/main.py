a4ae935732f56b8b19072680d8043e0a31ef0edb
John Baumert <baumert.john@gmail.com>
2025-05-27 19:46:27 -0600

    Create file write function. Add list of characters to not use in password. Create basic text UI for testing.

--- password_generator_gui/main.py
+++ password_generator_gui/main.py
@@ -7,20 +7,39 @@ import random
 
 
 def create_password(length):
+    NOUSE_CHARS = "<>'\""
     password = ""
-    for _ in range(length):
+    while len(password) < length:
         pass_char = chr(random.randint(33, 122))
+        if pass_char in NOUSE_CHARS:
+            continue
         password += pass_char
     return password
 
 
-def write_to_file(passwords):
-    pass
+def write_to_file(description, password):
+    if len(description) == 0:
+        description = "No Description"
+    with open("passwords.txt", "a") as file:
+        file.write(f"{description}: {password}\n")
 
 
 def main():
-    pass_length = int(input("Enter integer for password length: "))
-    print(create_password(pass_length))
+    print("Password Generator")
+    print("Password Length")
+    pass_length = int(input("> "))
+    print("Description (optional):")
+    description = input("> ")
+    _ = input("Press ENTER to Generate Password")
+    password = create_password(pass_length)
+    print("Generated Password")
+    print(password)
+    _ = input("Press ENTER to Save Password")
+    write_to_file(description, password)
+    # Verify file write
+    print("\nPassword File:")
+    with open("passwords.txt", "r") as file:
+        print(file.read())
 
 
 if __name__ == "__main__":
