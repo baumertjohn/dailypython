ed57b792ffbcdeb2f3c449e049d1cbd2de0d4582
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2025-05-30 12:52:08 -0600

    Create my_gui.py to do some PyQt5 testing/learning.

---
+++ password_generator_gui/my_gui.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.QtCore import Qt
+
+
+def main():
+    # Create an application object
+    app = QApplication(sys.argv)
+
+    # Create a basic window (QWidget)
+    window = QWidget()
+    window.setWindowTitle("My First PyQt5 GUI")
+    window.setGeometry(100, 100, 400, 200)  # (x, y, width, height)
+
+    # Create and position a QLabel widget
+    static_text_label = QLabel("Enter your name:", window)
+    static_text_label.move(50, 50)  # (x, y) coords relative to the windows top-left
+
+    # Create and position a QLineEdit widget
+    name_input = QLineEdit(window)
+    name_input.move(180, 50)
+    name_input.setFixedWidth(150)
+
+    # Create and position QPushButton
+    process_button = QPushButton("Process Test", window)
+    process_button.move(150, 100)
+
+    #window1 = QWidget()
+    #window1.setWindowTitle("My Second PyQt5 GUI")
+    #window1.setGeometry(1000, 600, 400, 200)  # (x, y, width, height)
+
+
+    # 3. Show the window
+    window.show()
+    #window1.show()
+
+    # 4. Start the application's event loop
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
