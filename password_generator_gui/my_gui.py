d394e43a1ec2a31e3f2a9247768de7dd41b7aa87
John Baumert <baumert.john@gmail.com>
2025-05-31 07:10:28 -0600

    Moved window to class to provide button functionality.

--- password_generator_gui/my_gui.py
+++ password_generator_gui/my_gui.py
@@ -1,34 +1,55 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout,
+)
+
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("My First PyQt5 GUI")
+        self.setGeometry(100, 100, 400, 200)  # (x, y, width, height)
+
+        layout = QVBoxLayout()
+
+        # Label
+        self.static_text_label = QLabel("Enter your name:")
+        layout.addWidget(self.static_text_label)  # Add label to layout
+
+        # Input Box
+        self.name_input = QLineEdit()
+        self.name_input.setPlaceholderText("Type your name here...")
+        layout.addWidget(self.name_input)
+
+        # Button
+        self.process_button = QPushButton("Process Text")
+        layout.addWidget(self.process_button)
+
+        # Connect the button "clicked" signal to "process_text" slot
+        self.process_button.clicked.connect(self.process_text)
+
+        self.setLayout(layout)
+
+    def process_text(self):
+        # Slot function
+        input_text = self.name_input.text()
+        print(f"Processed text: {input_text}")
 
 
 def main():
     # Create an application object
     app = QApplication(sys.argv)
-
-    # Create a basic window (QWidget)
-    window = QWidget()
-    window.setWindowTitle("My First PyQt5 GUI")
-    window.setGeometry(100, 100, 400, 200)  # (x, y, width, height)
-
-    # Create and position a QLabel widget
-    static_text_label = QLabel("Enter your name:", window)
-    static_text_label.move(50, 50)  # (x, y) coords relative to the windows top-left
-
-    # Create and position a QLineEdit widget
-    name_input = QLineEdit(window)
-    name_input.move(180, 50)
-    name_input.setFixedWidth(150)
-
-    # Create and position QPushButton
-    process_button = QPushButton("Process Test", window)
-    process_button.move(150, 100)
-
-    # 3. Show the window
-    window.show()
-
-    # 4. Start the application's event loop
-    sys.exit(app.exec_())
+    window = MyWindow()
+    window.show()  # Show the window
+    sys.exit(app.exec_())  # Start the event application's event loop
 
 
 if __name__ == "__main__":
